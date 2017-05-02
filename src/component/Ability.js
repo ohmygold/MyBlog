5ad5a7e3ba43ee2de7a92e9de657c9537f8fc12f
ohmygold <499452621@qq.com>
2017-05-02 23:50:00 +0800

    999

--- src/component/Ability.js
+++ src/component/Ability.js
@@ -13,7 +13,19 @@ class Ability extends React.Component {
           <p className='p_4'>电话：15555514149</p>
           <p className='p_5'>邮箱：499452621@qq.com</p>
         </div>
-        <div className='right'>666</div>
+        <div className='right'>
+          <div className='txt'>精通HTML5+CSS3、DIV+CSS等网站前端技术； <br/>
+          能够熟练编写标准前端代码，熟悉浏览器兼容问题解决方案；
+          </div>
+          <div className='txt'>熟练掌握javascript语言，精通jQuery库、bootstrap库、<br/>
+          Ant、Material-UI库，能够实现网页的动态效果与页面交互，<br />
+          可以使用插件完成特效；</div>
+          <div className='txt'>
+            了解node.js，掌握ajax，react，redux，熟悉ES6标准，有babel经验；<br />
+            熟练使用gulp、webpack自动化构建工具，了解sass，echarts图表库；<br />
+            熟悉基本的MySql，MongDB数据库的使用。
+          </div>
+        </div>
       </div>
     )
   }
--- src/component/Project.js
+++ src/component/Project.js
@@ -7,7 +7,18 @@ class Project extends React.Component {
   render() {
     return(
       <div className='project_1'>
-
+        <h2>项目经验</h2>
+        <div>
+          魔赞商城：（移动端）<a href='http://morethan.cc'>http://morethan.cc</a><br />
+          车融资：<a href='https://www.cherongzi.com'>https://www.cherongzi.com/</a><br />
+          视业网：<a href='www.shiyenet.com.cn'>www.shiyenet.com.cn</a><br />
+          研究生教育信息网：<a href='http://m.eduei.com'>http://m.eduei.com</a><br />
+          我爱购物网：<a href='http://m.55bbs.com'>http://m.55bbs.com</a><br />
+          新兴达机械：<a href='www.xxd999.com'>www.xxd999.com</a><br />
+          铁峰映像：<a href='www.tiefengimage.com'>www.tiefengimage.com</a><br />
+          个人博客：React+Webpack+React Router + Redux <br />
+          实现单页面应用 <a href='https://xwxsummer.github.io/08-demo'>https://xwxsummer.github.io/08-demo</a>
+        </div>
       </div>
     )
   }
